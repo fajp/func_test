b7c7de693327453ea5379725671b8990044cc69a
DESKTOP-5647LPU\User <fil.piech@gmail.com>
2022-04-26 23:23:56 +0200

    fist commit: func module, json file

---
+++ func.py
@@ -0,0 +1,23 @@
+def unique_substr_len(string: str) -> int:
+    """
+    Find the length of the longest substring
+    without repeating characters in the given string
+    """
+
+    chars = []
+    length = 0
+    max_length = 0
+
+    for char in string:
+
+        if char in chars:
+            length = 0
+            chars.clear()
+
+        chars.append(char)
+        length += 1
+        if length > max_length: max_length = length
+
+    return max_length
+
+print(unique_substr_len("""abcdef"""))
---
+++ main.py
@@ -0,0 +1,5 @@
+import json
+import func
+
+io = json.load(open('io.json', 'r'))
+print(io)
